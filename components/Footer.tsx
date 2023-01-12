2c6da54100812fb39e992ca8c464fe3949b22434
= <albin.groen@demando.se>
2023-01-12 15:34:01 +0100

    Update footer to use dynamic year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="flex items-center justify-between mt-16">
-      <p className="text-neutral-500">© 2022 Albin Groen</p>
+      <p className="text-neutral-500">
+        © {new Date().getFullYear()} Albin Groen
+      </p>
     </footer>
   );
 }
