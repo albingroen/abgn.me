797cec92ef1f53cba9b8e2704764d95fd4e0ac72
Albin Groen <albin.groen@gmail.com>
2023-05-24 14:04:42 +0200

    Add email to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,14 @@
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-between mt-16">
-      <p className="text-neutral-500">
+    <footer className="flex flex-col gap-8 items-center justify-between mt-16">
+      <a
+        href="mailto:albin.groen@gmail.com"
+        className="text-3xl sm:text-4xl hover:underline text-center"
+      >
+        albin.groen@gmail.com
+      </a>
+
+      <p className="text-neutral-500 text-center">
         © {new Date().getFullYear()} Albin Groen
       </p>
     </footer>
