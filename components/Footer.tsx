02a1e930e6225ea48d6c672377660fbada2f224e
Albin Groen <albin.groen@gmail.com>
2024-03-26 10:52:53 +0100

    Update email address

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,10 @@ export default function Footer() {
   return (
     <footer className="flex flex-col gap-8 items-center justify-between mt-16">
       <a
-        href="mailto:albin.groen@gmail.com"
+        href="mailto:albingroen@proton.me"
         className="text-3xl sm:text-4xl hover:underline text-center"
       >
-        albin.groen@gmail.com
+        albingroen@proton.me
       </a>
 
       <p className="text-neutral-500 text-center">
