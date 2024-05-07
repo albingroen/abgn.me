7e5a04fb29f18aa9195fd83dfe3877fe569ced8c
Albin Groen <albin.groen@gmail.com>
2024-05-07 14:37:51 +0200

    refactor: update profile image

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 export const PROFILE_PHOTO =
-  "https://res.cloudinary.com/albin-groen/image/upload/f_auto,q_auto/v1675928087/FD3034B7-B03C-4011-9BE5-237623585825-2342-0000023FADD1BFC2_2_x8a0xe.jpg";
+  "https://res.cloudinary.com/albin-groen/image/upload/f_auto,q_auto/v1715085340/profile-new-2_kcqmtf.jpg";
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
