cbf1667f6b27e5e60c72d6ccc334485665a9e4dc
Albin Groen <albin.groen@gmail.com>
2026-04-13 16:56:41 +0200

    build: update lockfile

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,97 +8,97 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as rootRouteImport } from "./routes/__root";
-import { Route as UsesRouteImport } from "./routes/uses";
-import { Route as IndexRouteImport } from "./routes/index";
-import { Route as PostsSlugRouteImport } from "./routes/posts.$slug";
-import { Route as InspirationCategoryRouteImport } from "./routes/inspiration.$category";
+import { Route as rootRouteImport } from './routes/__root'
+import { Route as UsesRouteImport } from './routes/uses'
+import { Route as IndexRouteImport } from './routes/index'
+import { Route as PostsSlugRouteImport } from './routes/posts.$slug'
+import { Route as InspirationCategoryRouteImport } from './routes/inspiration.$category'
 
 const UsesRoute = UsesRouteImport.update({
-  id: "/uses",
-  path: "/uses",
+  id: '/uses',
+  path: '/uses',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const IndexRoute = IndexRouteImport.update({
-  id: "/",
-  path: "/",
+  id: '/',
+  path: '/',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const PostsSlugRoute = PostsSlugRouteImport.update({
-  id: "/posts/$slug",
-  path: "/posts/$slug",
+  id: '/posts/$slug',
+  path: '/posts/$slug',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const InspirationCategoryRoute = InspirationCategoryRouteImport.update({
-  id: "/inspiration/$category",
-  path: "/inspiration/$category",
+  id: '/inspiration/$category',
+  path: '/inspiration/$category',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 
 export interface FileRoutesByFullPath {
-  "/": typeof IndexRoute;
-  "/uses": typeof UsesRoute;
-  "/inspiration/$category": typeof InspirationCategoryRoute;
-  "/posts/$slug": typeof PostsSlugRoute;
+  '/': typeof IndexRoute
+  '/uses': typeof UsesRoute
+  '/inspiration/$category': typeof InspirationCategoryRoute
+  '/posts/$slug': typeof PostsSlugRoute
 }
 export interface FileRoutesByTo {
-  "/": typeof IndexRoute;
-  "/uses": typeof UsesRoute;
-  "/inspiration/$category": typeof InspirationCategoryRoute;
-  "/posts/$slug": typeof PostsSlugRoute;
+  '/': typeof IndexRoute
+  '/uses': typeof UsesRoute
+  '/inspiration/$category': typeof InspirationCategoryRoute
+  '/posts/$slug': typeof PostsSlugRoute
 }
 export interface FileRoutesById {
-  __root__: typeof rootRouteImport;
-  "/": typeof IndexRoute;
-  "/uses": typeof UsesRoute;
-  "/inspiration/$category": typeof InspirationCategoryRoute;
-  "/posts/$slug": typeof PostsSlugRoute;
+  __root__: typeof rootRouteImport
+  '/': typeof IndexRoute
+  '/uses': typeof UsesRoute
+  '/inspiration/$category': typeof InspirationCategoryRoute
+  '/posts/$slug': typeof PostsSlugRoute
 }
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath;
-  fullPaths: "/" | "/uses" | "/inspiration/$category" | "/posts/$slug";
-  fileRoutesByTo: FileRoutesByTo;
-  to: "/" | "/uses" | "/inspiration/$category" | "/posts/$slug";
-  id: "__root__" | "/" | "/uses" | "/inspiration/$category" | "/posts/$slug";
-  fileRoutesById: FileRoutesById;
+  fileRoutesByFullPath: FileRoutesByFullPath
+  fullPaths: '/' | '/uses' | '/inspiration/$category' | '/posts/$slug'
+  fileRoutesByTo: FileRoutesByTo
+  to: '/' | '/uses' | '/inspiration/$category' | '/posts/$slug'
+  id: '__root__' | '/' | '/uses' | '/inspiration/$category' | '/posts/$slug'
+  fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute;
-  UsesRoute: typeof UsesRoute;
-  InspirationCategoryRoute: typeof InspirationCategoryRoute;
-  PostsSlugRoute: typeof PostsSlugRoute;
+  IndexRoute: typeof IndexRoute
+  UsesRoute: typeof UsesRoute
+  InspirationCategoryRoute: typeof InspirationCategoryRoute
+  PostsSlugRoute: typeof PostsSlugRoute
 }
 
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    "/uses": {
-      id: "/uses";
-      path: "/uses";
-      fullPath: "/uses";
-      preLoaderRoute: typeof UsesRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/": {
-      id: "/";
-      path: "/";
-      fullPath: "/";
-      preLoaderRoute: typeof IndexRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/posts/$slug": {
-      id: "/posts/$slug";
-      path: "/posts/$slug";
-      fullPath: "/posts/$slug";
-      preLoaderRoute: typeof PostsSlugRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/inspiration/$category": {
-      id: "/inspiration/$category";
-      path: "/inspiration/$category";
-      fullPath: "/inspiration/$category";
-      preLoaderRoute: typeof InspirationCategoryRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
+    '/uses': {
+      id: '/uses'
+      path: '/uses'
+      fullPath: '/uses'
+      preLoaderRoute: typeof UsesRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/posts/$slug': {
+      id: '/posts/$slug'
+      path: '/posts/$slug'
+      fullPath: '/posts/$slug'
+      preLoaderRoute: typeof PostsSlugRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/inspiration/$category': {
+      id: '/inspiration/$category'
+      path: '/inspiration/$category'
+      fullPath: '/inspiration/$category'
+      preLoaderRoute: typeof InspirationCategoryRouteImport
+      parentRoute: typeof rootRouteImport
+    }
   }
 }
 
@@ -107,16 +107,16 @@ const rootRouteChildren: RootRouteChildren = {
   UsesRoute: UsesRoute,
   InspirationCategoryRoute: InspirationCategoryRoute,
   PostsSlugRoute: PostsSlugRoute,
-};
+}
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>();
+  ._addFileTypes<FileRouteTypes>()
 
-import type { getRouter } from "./router.tsx";
-import type { createStart } from "@tanstack/react-start";
-declare module "@tanstack/react-start" {
+import type { getRouter } from './router.tsx'
+import type { createStart } from '@tanstack/react-start'
+declare module '@tanstack/react-start' {
   interface Register {
-    ssr: true;
-    router: Awaited<ReturnType<typeof getRouter>>;
+    ssr: true
+    router: Awaited<ReturnType<typeof getRouter>>
   }
 }
